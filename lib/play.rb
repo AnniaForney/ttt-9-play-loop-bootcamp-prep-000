a3e135ad60199853728e770a6bec586d9f17fcf1
Annia Forney <anniaforney@gmail.com>
2019-04-06 23:04:04 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,3 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -11,16 +10,14 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 end
 
 def turn(board)
@@ -35,4 +32,10 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
